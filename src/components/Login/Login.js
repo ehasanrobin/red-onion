34686c6b920e63e58cd1724075a36696edafe6f5
ehasanrobin <ehasanrobin44@gmail.com>
2022-08-19 17:53:50 +0600

    dfdsfs

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -26,6 +26,7 @@ const Login = () => {
     event.preventDefault();
     const email = event.target.email.value;
     const password = event.target.password.value;
+    
     signInWithEmailAndPassword(email, password)
    
   }
