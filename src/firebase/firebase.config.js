5ea3b6828bc188ca1a0ffca95c797f653d2ce776
ehasanrobin <ehasanrobin44@gmail.com>
2022-08-25 13:04:14 +0600

    red onion with firebase

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,11 +1,11 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyDeVpIMNwW8OSC26il59txSrnhJp2-ax4g",
-    authDomain: "red-onion-5164f.firebaseapp.com",
-    projectId: "red-onion-5164f",
-    storageBucket: "red-onion-5164f.appspot.com",
-    messagingSenderId: "947425451123",
-    appId: "1:947425451123:web:ce01b7be51672654e05e06",
-    measurementId: "G-BV3Y0LFY5F"
-  };
+  apiKey: "AIzaSyDeVpIMNwW8OSC26il59txSrnhJp2-ax4g",
+  authDomain: "red-onion-5164f.firebaseapp.com",
+  projectId: "red-onion-5164f",
+  storageBucket: "red-onion-5164f.appspot.com",
+  messagingSenderId: "947425451123",
+  appId: "1:947425451123:web:ce01b7be51672654e05e06",
+  measurementId: "G-BV3Y0LFY5F"
+};
 
   export default firebaseConfig;
